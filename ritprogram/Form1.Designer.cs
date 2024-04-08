7befd0f2805ab64a6dfccceb51666a938f56b961
Goalkee32 <goalkee32@gmail.com>
2024-04-08 20:11:33 +0200

    Add project files.

---
+++ ritprogram/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ritprogram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSudd = new System.Windows.Forms.Button();
+            this.btnEllips = new System.Windows.Forms.Button();
+            this.btnRektangel = new System.Windows.Forms.Button();
+            this.tbxStorlek = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPenna = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(577, 425);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(597, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 38);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 39);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnSudd
+            // 
+            this.btnSudd.Image = ((System.Drawing.Image)(resources.GetObject("btnSudd.Image")));
+            this.btnSudd.Location = new System.Drawing.Point(708, 67);
+            this.btnSudd.Name = "btnSudd";
+            this.btnSudd.Size = new System.Drawing.Size(70, 70);
+            this.btnSudd.TabIndex = 2;
+            this.btnSudd.UseVisualStyleBackColor = true;
+            this.btnSudd.Click += new System.EventHandler(this.btnSudd_Click);
+            // 
+            // btnEllips
+            // 
+            this.btnEllips.Image = ((System.Drawing.Image)(resources.GetObject("btnEllips.Image")));
+            this.btnEllips.Location = new System.Drawing.Point(619, 158);
+            this.btnEllips.Name = "btnEllips";
+            this.btnEllips.Size = new System.Drawing.Size(70, 70);
+            this.btnEllips.TabIndex = 2;
+            this.btnEllips.UseVisualStyleBackColor = true;
+            this.btnEllips.Click += new System.EventHandler(this.btnEllips_Click);
+            // 
+            // btnRektangel
+            // 
+            this.btnRektangel.Image = ((System.Drawing.Image)(resources.GetObject("btnRektangel.Image")));
+            this.btnRektangel.Location = new System.Drawing.Point(708, 158);
+            this.btnRektangel.Name = "btnRektangel";
+            this.btnRektangel.Size = new System.Drawing.Size(70, 70);
+            this.btnRektangel.TabIndex = 2;
+            this.btnRektangel.UseVisualStyleBackColor = true;
+            this.btnRektangel.Click += new System.EventHandler(this.btnRektangel_Click);
+            // 
+            // tbxStorlek
+            // 
+            this.tbxStorlek.Location = new System.Drawing.Point(692, 258);
+            this.tbxStorlek.Name = "tbxStorlek";
+            this.tbxStorlek.Size = new System.Drawing.Size(100, 26);
+            this.tbxStorlek.TabIndex = 3;
+            this.tbxStorlek.Text = "5";
+            this.tbxStorlek.TextChanged += new System.EventHandler(this.tbxStorlek_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(596, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Linje storlek";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(692, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPenna
+            // 
+            this.btnPenna.Image = ((System.Drawing.Image)(resources.GetObject("btnPenna.Image")));
+            this.btnPenna.Location = new System.Drawing.Point(619, 67);
+            this.btnPenna.Name = "btnPenna";
+            this.btnPenna.Size = new System.Drawing.Size(70, 70);
+            this.btnPenna.TabIndex = 2;
+            this.btnPenna.UseVisualStyleBackColor = true;
+            this.btnPenna.Click += new System.EventHandler(this.btnPenna_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxStorlek);
+            this.Controls.Add(this.btnRektangel);
+            this.Controls.Add(this.btnEllips);
+            this.Controls.Add(this.btnSudd);
+            this.Controls.Add(this.btnPenna);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSudd;
+        private System.Windows.Forms.Button btnEllips;
+        private System.Windows.Forms.Button btnRektangel;
+        private System.Windows.Forms.TextBox tbxStorlek;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPenna;
+    }
+}
+
---
+++ ritprogram/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Deployment.Application;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ritprogram
+{
+    public partial class Form1 : Form
+    {
+
+        public grej grej = new grej();
+        
+        public Form1()
+        {
+            // Initierar programmet. Sätter start pennan till färgen svart och storlek 5. Gör även pennans dragning 
+            // smooth och rund.
+            InitializeComponent();
+            grej.Initialize(panel1);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            grej.pbx1(sender);
+
+        }
+
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            grej.Down(e, panel1);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            grej.Draw(e);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            grej.Up(e, panel1);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPenna_Click(object sender, EventArgs e)
+        {
+            grej.Penna(pictureBox1, tbxStorlek);
+        }
+
+        private void tbxStorlek_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSudd_Click(object sender, EventArgs e)
+        {
+            grej.Sudd(pictureBox1, tbxStorlek);
+        }
+
+        private void btnEllips_Click(object sender, EventArgs e)
+        {
+            grej.Ellipse();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            grej.btn1(tbxStorlek);
+        }
+
+        private void btnRektangel_Click(object sender, EventArgs e)
+        {
+            grej.Rektangel();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    public class grej
+    {
+        Graphics g;
+        private bool moving = false;
+        private bool penactive = true;
+        private bool rectangle = false;
+        private bool ellipse = false;
+        Pen pen;
+        private int x = -1;
+        private int y = -1;
+
+        public void Up(MouseEventArgs e, Panel panel1)
+        {
+            // Om musen rörs bort från ritfältet avaktiveras moving boolen och muspekaren sätts till default igen.
+            moving = false;
+            x = e.X;
+            y = e.Y;
+            panel1.Cursor = Cursors.Default;
+        }
+
+        public void Down(MouseEventArgs e, Panel panel1)
+        {
+            // När musen rörs till ritfältet aktiveras moving boolen och x samt y sätts till muspekarens punkt. Muspekaren ändras även till ett kors. 
+            moving = true;
+            x = e.X;
+            y = e.Y;
+            panel1.Cursor = Cursors.Cross;
+        }
+
+        public void Draw(MouseEventArgs e)
+        {
+            // När musen rörs händer olika saker beroende på vad användaren valt för målarverktyg. 
+            Brush brush = new SolidBrush(pen.Color);
+            Rectangle rect = new Rectangle(x, y, x, y);
+            if (moving && penactive)
+            {
+                g.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+            if (moving && rectangle)
+            {
+                g.FillRectangle(brush, rect);
+                x = e.X;
+                y = e.Y;
+            }
+            if (moving && ellipse)
+            {
+                g.FillEllipse(brush, rect);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        public void Penna(PictureBox pictureBox1, TextBox tbxStorlek)
+        {
+            // När Penna knappen trycks ner aktiveras pen verktyget och färgen samt storleken sätts till den angivna storleken.
+            rectangle = false;
+            ellipse = false;
+            penactive = true;
+            pen = new Pen(pen.Color, int.Parse(tbxStorlek.Text));
+            pictureBox1.BackColor = Color.Black;
+            pen.Color = pictureBox1.BackColor;
+        }
+
+        public void Sudd(PictureBox pictureBox1, TextBox tbxStorlek)
+        {
+            // När Sudd knappen trycks in byts färgen till vit på pennan, samma som bakgrunden.
+            rectangle = false;
+            ellipse = false;
+            penactive = true;
+            pen = new Pen(Color.White, int.Parse(tbxStorlek.Text));
+            pictureBox1.BackColor = Color.White;
+        }
+
+        public void Ellipse()
+        {
+            // När Ellips knappen klickas så aktiveras ellipsverktyget.
+            penactive = false;
+            rectangle = false;
+            ellipse = true;
+        }
+
+        public void btn1(TextBox tbxStorlek)
+        {
+            // Apply knappen för storleken på pennan. När denna klickas uppdateras pennans storlek.
+            pen = new Pen(pen.Color, int.Parse(tbxStorlek.Text));
+        }
+
+        public void Rektangel()
+        {
+            // När Rektangel knappen klickas så aktiveras rektangelverktyget. 
+            penactive = false;
+            ellipse = false;
+            rectangle = true;
+        }
+
+        public void pbx1(object sender)
+        {
+            // För att sätta färgen på pennan. Den färg som användaren klickar på sätts from pennans färg.
+            PictureBox p = (PictureBox)sender;
+            ColorDialog c = new ColorDialog();
+            if (c.ShowDialog() == DialogResult.OK)
+            {
+                p.BackColor = c.Color;
+
+
+            }
+            pen.Color = c.Color;
+        }
+
+        public void Initialize(Panel panel1)
+        {
+            g = panel1.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pen = new Pen(Color.Black, 5);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+    }
+
+}
